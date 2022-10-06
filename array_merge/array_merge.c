3374d06af51726489a50998792c908ea48f407ed
Max Quintavalle <quint218@normandy.morris.umn.edu>
2022-10-05 20:10:09 -0500

    Fix the memory leaks and fix print statements
    We deleted the print statments in the .c file as well as added some commments to make it more clear (I finally got max to stop being a horrible coder, thank me later). Then we went through and fixed all of the issues valgrind brings up.

--- array_merge/array_merge.c
+++ array_merge/array_merge.c
@@ -5,14 +5,15 @@
 
 // This assumes that the array of values is already sorted
 int count_unique(int* values, int size){
-
-int uniq = 1, tempIndex = values[0];
- //Hopefully this for loop counts the unique values
+// uniq is the counter for unique numbers and curruniq is the current uniq value
+// we start uniq at one since the base case is that the first one always is unique
+// curruniq follows the same idea
+int uniq = 1, curruniq = values[0];
 
 
   for(int i = 0; i < size; i++){
-	  if(tempIndex != values[i]){
-	    tempIndex = values[i];
+	  if(curruniq != values[i]){
+	    curruniq = values[i];
 	    uniq++;  
 	  }
 
@@ -25,7 +26,8 @@ int uniq = 1, tempIndex = values[0];
 
 int* array_merge(int num_arrays, int* sizes, int** values) {
   int total_size = 0;
-if(num_arrays == 0) return values[0] ;
+  // If there are no arrays then we don't run the function
+  if(num_arrays == 0) return values[0] ;
 	
   // Find the total size to initiate the final array
   for(int i = 0; i < num_arrays; i++){
@@ -34,23 +36,6 @@ if(num_arrays == 0) return values[0] ;
   // The actual allocation line for the final result
   int *destination =(int*)calloc(total_size, sizeof(int));
 
-
-// Print statement to see all of the elements of the input values
-  printf("---------------------Elements of the multi-dim array----------------- \n");    
-      //Loop through the array by incrementing value of i
-      for(int j = 0; j < num_arrays; j++){     
-          for (int i = 0; i < sizes[j]; i++) {     
-                  printf("This is row %d column %d, value : %d\n", j, i, values[j][i]);	
-                      }
-      }
-
-// Print statement to see all of the values of the input sizes
-  printf("-------------------List of Sizes for each array------------------------ \n"); 
-      // Navigate through the sizes array:
-      for(int i = 0; i < num_arrays; i++) {
-      printf("Position %d in sizes: %d\n", i, sizes[i]);
-      }
-
 // This iterates through all of the elements of the multi-array and puts it all into
 // the destination array
   int destIndex = 0;
@@ -58,41 +43,24 @@ if(num_arrays == 0) return values[0] ;
   	for(int i = 0; i < sizes[j]; i++){
 		destination[destIndex] = values[j][i];
 		destIndex++;
-	}
-
-	
+	}	
   }
-
-
-
-
-
+  // Sort the destination array, since our counter only works when it is already sorted
   mergesort(total_size , destination);
-
+  // Number of unique numbers
   int count = count_unique(destination, total_size);
-  printf("\nNumber of uniq: %d", count);
+  // The final goal
+  int *endgoal = (int*)calloc(count+1,sizeof(int));
+  // First element in the array is the total unique characters
+  endgoal[0] = count;
+  // This assumes the first element in destination is already unique
+  endgoal[1] = destination[0];
+  int curruniq = destination[0];
+  int index = 2;
 
 
-
-// This is the print statement to see what the values of destination are
-  printf("-------------------List of values in destination------------------------ \n");
-      // Navigate through the destination array and print:
-      for(int i = 0; i < total_size; i++) { 
-	      printf("Position %d in destination: %d\n", i, destination[i]);
-      }
-
-
-int *endgoal = (int*)calloc(count+1,sizeof(int));
-endgoal[0] = count;
-
-endgoal[1] = destination[0];
-
-int curruniq = destination[0];
-int index = 2;
-
- //Hopefully this for loop counts the unique values
-
-
+  // Here we iterate through and only add unique characters
+  // This only works when the destination array is already sorted
   for(int i = 0; i < total_size; i++){
           if(curruniq != destination[i]){
             curruniq = destination[i];
@@ -100,15 +68,6 @@ int index = 2;
             index++;
           } 
   }
-
-// This is the print statement to see what the values of destination are
-  printf("-------------------List of values in endgoal------------------------ \n");
-      // Navigate through the destination array and print:
-      for(int i = 0; i < count; i++) {
-              printf("Position %d in end goal: %d\n", i, endgoal[i]);
-      }
-
-
-
+  free(destination);
   return endgoal;
 }
--- array_merge/array_merge_test.cpp
+++ array_merge/array_merge_test.cpp
@@ -18,6 +18,7 @@ TEST(ArrayMerge, Handle_empty_list) {
 
   result = array_merge(0, sizes,  a);
   arrays_match(1, result, expected);
+  free(result);
 }
 
 TEST(ArrayMerge, Handle_singleton_list) {
@@ -30,6 +31,8 @@ TEST(ArrayMerge, Handle_singleton_list) {
 
   result = array_merge(num_arrays, sizes, a);
   arrays_match(2, result, expected);
+  free(result);
+
 }
 
 TEST(ArrayMerge, Handle_one_longer_list) {
@@ -42,6 +45,8 @@ TEST(ArrayMerge, Handle_one_longer_list) {
 
   result = array_merge(num_arrays, sizes, a);
   arrays_match(8, result, expected);
+  free(result);
+
 }
 
 TEST(ArrayMerge, Handle_multiple_copies_of_longer_list) {
@@ -54,6 +59,8 @@ TEST(ArrayMerge, Handle_multiple_copies_of_longer_list) {
 
   result = array_merge(num_arrays, sizes, a);
   arrays_match(8, result, expected);
+  free(result);
+
 }
 
 TEST(ArrayMerge, Handle_multiple_copies_of_longer_list_different_orders) {
@@ -68,6 +75,8 @@ TEST(ArrayMerge, Handle_multiple_copies_of_longer_list_different_orders) {
 
   result = array_merge(num_arrays, sizes, a);
   arrays_match(8, result, expected);
+  free(result);
+
 }
 
 TEST(ArrayMerge, Handle_different_sizes) {
@@ -84,10 +93,18 @@ TEST(ArrayMerge, Handle_different_sizes) {
     for (j=0; j<i; ++j) {
       a[i][j] = j;
     }
+
   }
 
   result = array_merge(num_arrays, sizes, a);
   arrays_match(11, result, expected);
+  for (i=0; i<num_arrays; ++i) {
+   
+   free(a[i]);
+  }
+
+  free(result);
+
 }
 
 TEST(ArrayMerge, Handle_different_sizes_reversed) {
@@ -108,6 +125,13 @@ TEST(ArrayMerge, Handle_different_sizes_reversed) {
 
   result = array_merge(num_arrays, sizes, a);
   arrays_match(11, result, expected);
+  for (i=0; i<num_arrays; ++i) {
+    
+   free(a[i]);
+  }
+
+  free(result);
+
 }
 
 int main(int argc, char* argv[]) {
